94d56c8dc0e10353e61374e9f032cfdf8b08a01b
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-05-02 22:05:01 +0600

    mongodb intialized and inventory car API created

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -7,15 +9,38 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json())
 
-const users = [
-    { name: 'Salman Khan', email: 'salman@khan.com' },
-    { name: 'Amir Khan', email: 'amir@khan.com' },
-    { name: 'Nawaj Ali Siddiki', email: 'nawaj@siddiki.com' },
-];
 
-app.get('/users', (req, res) => {
-    res.send(users);
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bytmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const carCollection = client.db('inventory').collection('car');
+
+        // Car API
+        app.get('/car', async (req, res) => {
+            const query = {};
+            const cursor = carCollection.find(query);
+            const cars = await cursor.toArray();
+            res.send(cars);
+        })
+
+        app.get('/car/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) }
+            const car = await carCollection.findOne(query);
+            res.send(car)
+        })
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('This is our car ghoor API');
