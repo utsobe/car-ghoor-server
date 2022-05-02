38da02453ecd236e95300c224a0cc8849dd30039
devilmamuncn@gmail.com <mmhutsobe01@gmail.com>
2022-05-02 19:44:51 +0600

    intialize express js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// use middleware 
+app.use(cors());
+app.use(express.json())
+
+const users = [
+    { name: 'Salman Khan', email: 'salman@khan.com' },
+    { name: 'Amir Khan', email: 'amir@khan.com' },
+    { name: 'Nawaj Ali Siddiki', email: 'nawaj@siddiki.com' },
+];
+
+app.get('/users', (req, res) => {
+    res.send(users);
+})
+
+app.get('/', (req, res) => {
+    res.send('This is our car ghoor API');
+});
+
+app.listen(port, () => {
+    console.log('The server running by PORT', port);
+})
